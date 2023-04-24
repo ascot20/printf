767474a586e6d2ab3d8d1f6e8e243df9ed757e68
ascot20 <danielotchere18@gmail.com>
2023-04-24 09:40:40 -0700

    added print_binary file

---
+++ print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_binary-print unsigned int to binary
+ * @b: va_list array
+ */
+int print_binary(va_list b)
+{
+	unsigned int num = va_arg(b, unsigned int);
+	unsigned int count = 0;
+	int i;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	for (i = 31; i >= 0; i--)
+	{
+		if (num & (1 << i))
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count > 0)
+		{
+			_putchar('0');
+			count++;
+		}
+	}
+
+	return (count);
+}
