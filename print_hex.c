0c295940660c25a8e51234d185e40cc7526d4524
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-25 09:55:21 +0000

    Update print_hex.c

--- print_hex.c
+++ print_hex.c
@@ -7,31 +7,43 @@
  */
 int print_hex(unsigned int u, char c)
 {
-	const char *hex_digits = "0123456789abcdef0123456789ABCDEF";
-	char buffer[50];
-	char *ptr = &buffer[49];
+	unsigned int len, powten, j, digit, num;
 	int count = 0;
+	char diff;
 
-	if (u == 0)
+	if (u != 0)
+	{
+		num = u;
+		len = 0;
+		if (c)
+			diff = 'A' - ':';
+		else
+			diff = 'a' - ':';
+		while (num != 0)
+		{
+			num /= 16;
+			len++;
+		}
+		powten = 1;
+		for (j = 1; j <= len - 1; j++)
+			powten *= 16;
+		for (j = 1; j <= len; j++)
+		{
+			digit = u / powten;
+			if (digit < 10)
+				_putchar(digit + '0');
+			else
+				_putchar(digit + '0' + diff);
+			count++;
+			u -= digit * powten;
+			powten /= 16;
+		}
+	}
+	else
 	{
 		_putchar('0');
 		return (1);
 	}
-
-	*ptr = '\0';
-
-	while (u != 0)
-	{
-		*--ptr = hex_digits[u % 16 + (c ? 16 : 0)];
-		u /= 16;
-	}
-	while (*ptr != '\0')
-	{
-		_putchar(*ptr);
-		ptr++;
-		count++;
-	}
-
 	return (count);
 }
 
@@ -42,7 +54,7 @@ int print_hex(unsigned int u, char c)
  */
 int print_hexa(va_list args)
 {
-	return (print_hex(va_arg(args, unsigned int), 'a'));
+	return (print_hex(va_arg(args, unsigned int), 0));
 }
 
 /**
@@ -52,5 +64,5 @@ int print_hexa(va_list args)
  */
 int print_hexA(va_list args)
 {
-	return (print_hex(va_arg(args, unsigned int), 'A'));
+	return (print_hex(va_arg(args, unsigned int), 1));
 }
